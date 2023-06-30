4b2e4b66afd102381754e8a9280847f84393a935
juhyee <vlel3657@naver.com>
2023-06-30 16:23:43 +0900

    DNV publishing_Outsourcing

--- DNV/js/ui.js
+++ DNV/js/ui.js
@@ -2,20 +2,20 @@ $(function() {
   //quick
   var asidePos = parseInt($(".aside").css("top"));
   var asideBtn = $(".btn_aside")
-
+  var header = $(".header")
   $(window).scroll(function(){
       var scT = $(window).scrollTop();
       // var conH = $(window).height();
-
       if(scT === 0){
         $(".aside").stop().animate({'top':scT + asidePos + "px"},600);
+        header.removeClass('isScroll')
       }
       else{
         $(".aside").stop().animate({'top':scT + asidePos + "px"},600);
+        header.addClass('isScroll')
       }
 
   });
-
   asideBtn.on('click', function(){
     $("body, html").animate({ scrollTop: 0 }, "slow");
   })
