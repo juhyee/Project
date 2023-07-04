883f2952ddf2ec7e3107f4408f617732f27688ff
juhyee <vlel3657@naver.com>
2023-07-04 11:03:37 +0900

    DNV publishing_Outsourcing

--- DNV/js/ui.js
+++ DNV/js/ui.js
@@ -1,20 +1,24 @@
 $(function() {
   //quick
   var asidePos = parseInt($(".aside").css("top"));
-  var asideBtn = $(".btn_aside")
+  var asideBtn = $(".btn-aside")
   var header = $(".header")
   $(window).scroll(function(){
       var scT = $(window).scrollTop();
-      // var conH = $(window).height();
-      if(scT === 0){
-        $(".aside").stop().animate({'top':scT + asidePos + "px"},600);
-        header.removeClass('isScroll')
-      }
-      else{
-        $(".aside").stop().animate({'top':scT + asidePos + "px"},600);
+      var visaulH = $('.secSubVisual').height();
+      var conH = $('.container').height();
+      var asideH = $('.aside').height();
+      var footerH = $('.footer').height();
+      if(visaulH  > scT){
+        $(".aside").stop().animate({'top':asidePos + "px"},400);
+        // header.removeClass('isScroll')
+      }else if(conH - asideH < scT){
+        $(".aside").stop().animate({'top': conH - asideH - 100 + 'px'},400);
+      }else{
+        $(".aside").stop().animate({'top':scT + 40  + "px"},400);
         header.addClass('isScroll')
       }
-
+      // $('#header').css({left: -$(window).scrollLeft()});
   });
   asideBtn.on('click', function(){
     $("body, html").animate({ scrollTop: 0 }, "slow");
@@ -24,7 +28,25 @@ $(function() {
 
 
 var swiperPagination = new Swiper(".swiper_visual", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiperPagination = new Swiper(".swiperTumd", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
